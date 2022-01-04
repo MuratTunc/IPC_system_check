2e59144fd169e75f17a68d1d7741ac517ccf96ce
Murat Tunc <murat.tunc8558@gmail.com>
2022-01-04 13:27:45 +0300

    Added vnic interface...

--- scripts/Check_System.sh
+++ scripts/Check_System.sh
@@ -24,20 +24,19 @@ check_modules(){
     
     command lsmod | grep vnic
     
+    echo "-------------------------------------------"
+    echo "MAC interfaces..."
     #Get Linux and SWCPU mac numbers.
     command s7_vnic_macconfig -m /mnt/swcpu_mount
     MAC_ALL=$(s7_vnic_macconfig -m /mnt/swcpu_mount)
     SWCPU_MAC=${MAC_ALL:30:19}
-    echo "${SWCPU_MAC}"
 
     #Get all ip interfaces
-    echo "**************************"
     IP_A=$(ip a)
-    echo "${IP_A}"
+    #echo "${IP_A}"
     
-    echo "**************************"
     ALL_NETWORK_INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
-    echo "${ALL_NETWORK_INTERFACES}"
+    #echo "${ALL_NETWORK_INTERFACES}"
 
 
 
@@ -46,10 +45,9 @@ check_modules(){
 check_enp0s_exist(){
     SUB='enp0s'
     if [[ "$ALL_NETWORK_INTERFACES" == *"$SUB"* ]]; then
-       echo "It's there."
-       echo "**************************"
-       VNIC_INTERFACE=$(cut -d "$SUB" -f2 <<< "$ALL_NETWORK_INTERFACES")
-       echo "${VNIC_INTERFACE}"
+       VNIC_INTERFACE=$( echo "$ALL_NETWORK_INTERFACES" | tail -n1)
+       echo "-------------------------------------------"
+       echo "VNIC interface is found...${VNIC_INTERFACE}"
 
     fi
 }
